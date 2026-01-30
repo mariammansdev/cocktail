006497bf55ef77914ef7cd08eeeb8dbeeaf8c9d0
mariam mansour <mariummmansour19@gmail.com>
2026-01-30 12:58:51 +0100

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import gsap from "gsap";
 import Hero from "./components/Hero";
 import Cocktail from "./components/Cocktail";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktail />
+      <About />
     </main>
   )
 }
